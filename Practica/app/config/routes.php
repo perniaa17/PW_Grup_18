6cf5236ffb586737ca776cdfe9df55e19efd95c3
perniaa17 <mpcmarc@gmail.com>
2017-04-27 19:25:09 +0200

    Routes.php $before

--- Practica/app/config/routes.php
+++ Practica/app/config/routes.php
@@ -4,5 +4,18 @@ $app->get('/hello/{name}', 'SilexApp\Controller\HelloController::indexAction');
 // USER
 $app->get('/users/get/{id}', 'SilexApp\Controller\UserController::getAction');
 $app->match('/users/add', 'SilexApp\Controller\UserController::postAction');
+
+//SESSION
+$before = function(Request $request, Application $app){
+    if (!$app['session']->has('name')){
+        $response = new Response();
+        $content = $app['twig']->render('error.twig', [
+            'message' => 'You must be logged'
+        ]);
+        $response->setContent($content);
+        $response->setStatusCode(Response::HTTP_FORBIDDEN);
+        return $response;
+    }
+};
 $app->get('/', 'SilexApp\Controller\BaseController::indexAction');
 $app->get('/admin','SilexApp\Controller\BaseController::adminAction');
